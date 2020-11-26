932bd92a95523b965b257f2dc20012aca967286c
BobVoit <63784248+BobVoit@users.noreply.github.com>
2020-11-26 15:37:49 +0400

    add hooks

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './ProfileInfo.module.css';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
     return (
@@ -14,7 +15,7 @@ const ProfileInfo = (props) => {
             <div>
               <div>{props.profile.fullName}</div>
             </div>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
           </div>
         </div>
       </div>
