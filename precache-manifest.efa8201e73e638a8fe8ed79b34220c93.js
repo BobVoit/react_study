db1982b4345d2c81399c277d8a49e56a6f2a2592
BobVoit <63784248+BobVoit@users.noreply.github.com>
2020-12-21 17:04:01 +0400

    Updates

---
+++ precache-manifest.efa8201e73e638a8fe8ed79b34220c93.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adcdc8f917b2fb8717a46765d57a369",
+    "url": "/react_study/index.html"
+  },
+  {
+    "revision": "7d01e80ecf33c18527e1",
+    "url": "/react_study/static/css/3.a59b1721.chunk.css"
+  },
+  {
+    "revision": "e5e6e4b5b9cc7a67889e",
+    "url": "/react_study/static/css/4.41c10588.chunk.css"
+  },
+  {
+    "revision": "ede56545a66de33608c7",
+    "url": "/react_study/static/css/main.e4c57b5e.chunk.css"
+  },
+  {
+    "revision": "04477c3b4f9da6f286a6",
+    "url": "/react_study/static/js/2.5a580bbd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_study/static/js/2.5a580bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d01e80ecf33c18527e1",
+    "url": "/react_study/static/js/3.17bca331.chunk.js"
+  },
+  {
+    "revision": "e5e6e4b5b9cc7a67889e",
+    "url": "/react_study/static/js/4.7cab9d36.chunk.js"
+  },
+  {
+    "revision": "ede56545a66de33608c7",
+    "url": "/react_study/static/js/main.fd3773e0.chunk.js"
+  },
+  {
+    "revision": "0caf9ee4034ea9c2aa51",
+    "url": "/react_study/static/js/runtime-main.04fdc329.js"
+  },
+  {
+    "revision": "1d3a9efc31fc0ecc2189bcd76f70994e",
+    "url": "/react_study/static/media/avatar1.1d3a9efc.jpg"
+  },
+  {
+    "revision": "2163ddee6a06d68d5281156edcf38e3d",
+    "url": "/react_study/static/media/preloader.2163ddee.svg"
+  },
+  {
+    "revision": "b3e90dfeb6b3b2de52eddd2c94ab19ab",
+    "url": "/react_study/static/media/user.b3e90dfe.jpg"
+  }
+]);
